91d3d329698917d54abcabe361540da89d382fa7
BeomJuGo <lom0097@naver.com>
2025-03-21 19:41:28 +0900

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,20 +5,10 @@ export const fetchParts = async (category) => {
       { id: 1, name: "Intel Core i5-14600K", score: 90 },
       { id: 2, name: "Intel Core i9-14900K", score: 21 },
       { id: 3, name: "Intel Core i5-14400F", score: 31 },
-      { id: 4, name: "Intel Core Ultra7 265K", score: 51 },
-      { id: 5, name: "Intel Core Ultra9 285K", score: 61 },
-      { id: 6, name: "Intel Core Ultra5 225F", score: 11 },
-      { id: 7, name: "AMD Ryzen 5 7500F", score: 11 },
-      { id: 8, name: "AMD Ryzen 7 9600X", score: 11 },
-      { id: 9, name: "AMD Ryzen 7 9700X", score: 11 },
-      { id: 10, name: "AMD Ryzen 9 9950X", score: 11 },
     ],
     gpu: [
       { id: 1, name: "NVIDIA RTX 4070", score: 95 },
       { id: 2, name: "AMD Radeon RX 7900 XT", score: 92 },
-      { id: 3, name: "AMD Radeon RX 7900", score: 90 },
-      { id: 4, name: "RTX 5070", score: 90 },
-      { id: 5, name: "RTX 5080", score: 92 },
     ],
   };
 
